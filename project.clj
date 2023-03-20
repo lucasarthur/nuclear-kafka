dc5bc6d838427a37643b801a83a513254eb6d6ff
Lucas Arthur <luca.oetdbem@gmail.com>
2023-03-20 00:41:21 -0300

    initial load

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject nuclear-kafka "0.0.1"
+  :description "A Clojure wrapper with batteries for Reactor Kafka"
+  :url "https://github.com/lucasarthur/nuclear-kafka"
+  :license {:name "GNU General Public License v3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.pt-br.html"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [nuclear "0.1.2"]])
